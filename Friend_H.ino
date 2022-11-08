3f9334f0023d72ff6d0757819e2955636f03a76f
BossBoxing <boss_slam@hotmail.com>
2022-11-08 14:13:53 +0700

    add value

--- Friend_H.ino
+++ Friend_H.ino
@@ -1,3 +1,30 @@
+#include <ModelPro.h>
+
+#define S_FL analog(0)
+#define S_FR analog(1)
+#define S_R analog(2)
+#define S_G analog(3) 
+#define S_B analog(4)
+#define S_W analog(5)
+#define S_BL analog(6)
+#define S_BR analog(7)
+
+int reff_S_FL = 500;
+int reff_S_FR = 500;
+int reff_S_BL = 500;
+int reff_S_BR = 500;
+
+#define Servo_R 1
+#define Servo_G 2
+#define Servo_B 3
+#define Servo_Y 4
+#define Servo_Flag 5
+
+#define E_FL 1
+#define E_FR 2
+#define E_BL 18
+#define E_BR 19
+
 void setup() {
   // put your setup code here, to run once:
 
